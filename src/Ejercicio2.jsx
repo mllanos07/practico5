b0bd206db335f189a8bb10f08af898d4ff5e9de5
llanos <d47001835@alumnos.itr.edu.ar>
2024-07-02 22:57:04 -0300

    Formulario de nombre y edad

--- src/Ejercicio2.jsx
+++ src/Ejercicio2.jsx
@@ -1,9 +1,55 @@
+import { useState } from "react";
 export default function Ejercicio2() {
-  
+  const [formData, setFormData] = useState({
+    nombre: "",
+    edad: "",
+  });
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = () => {
+    if (formData.edad >= 18) {
+      window.alert('La persona ingresada es mayor de edad');
+    } else {
+      window.alert ('La persona ingresada es menor de edad');
+    }
+  };
+
   return (
     <div>
       <h2>Ejercicio 2: Formulario</h2>
-      {/* Crear formulario */}
+      <form>
+        <div>
+          <label>Nombre</label>
+          <input
+            type="text"
+            id="nombre"
+            name="nombre"
+            value={formData.nombre}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>edad</label>
+          <input
+            type="number"
+            id="edad"
+            name="edad"
+            value={formData.edad}
+            onChange={handleChange}
+          />
+        </div>
+      </form>
+      <button onClick={handleSubmit}>Enviar Formulario</button>
+      <p>
+        {formData.nombre} tiene {formData.edad} años
+      </p>
     </div>
-  )
+  );
 }
